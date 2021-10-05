1d0a34a6d2da76e61341b046db9b3855321106b4
Saumya Mani Tripathi <saumyamt25@gmail.com>
2021-10-05 15:42:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var txtinput = document.querySelector("#txt-input");
 var outputdiv =  document.querySelector("#output");
 
 function clickHandler(){
-    outputdiv.innerText = "DSDASDDAD" + txtinput.ariaValueMax;
+    outputdiv.innerText = "DSDASDDAD" + txtinput.value ;
+    
 }
 
 btnTranslate.addEventListener("click",clickHandler);
